be7f57b798b4d3ed68dc405e1727c317dedf8a16
lessmoon <lsmcsp@gmail.com>
2014-04-01 19:46:48 +0800

    remove some functions from the interfaces and pipex

--- src/pipe_api.h
+++ src/pipe_api.h
@@ -20,13 +20,14 @@ struct pipe_basic{
     {}
     virtual ~pipe_basic()
     {}
-
+    
+    virtual bool is_open()const;
     virtual int close_write() = 0;
     virtual int close_read() = 0;
     virtual int write_tie(TYPE) = 0;
     virtual int read_tie(TYPE) = 0;
-    virtual int write_tie_file(file_basic&) = 0;
-    virtual int read_tie_file(file_basic&) = 0;
+    //virtual int write_tie_file(file_basic&) = 0;
+    //virtual int read_tie_file(file_basic&) = 0;
 };//struct pipe_basic
 
 #endif //_PIPE_API_H
